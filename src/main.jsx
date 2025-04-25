3a7eaae60402c155d455b9e518a0829250775e34
csmanhaes86 <charlesmanhaes1@gmail.con>
2025-04-25 00:24:45 -0300

    estilos globais

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import GlobalStyles from './styles/globalStyles';
 
 import Router from './routes/routes';
 
@@ -10,5 +11,6 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Router />
     </BrowserRouter>
+    <GlobalStyles />
   </StrictMode>,
 );
